e27c6b68c3805ef5d578f22b72ba0a33b8f2513f
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-11-09 10:41:47 -0300

    usando o Drawer

--- flutter_nv1/lib/pages/home_page.dart
+++ flutter_nv1/lib/pages/home_page.dart
@@ -19,6 +19,55 @@ class _HomePageState extends State<HomePage> {
         title: Text("AppBar"),
         backgroundColor: Colors.green,
       ),
+      drawer: Drawer(
+        child: ListView(
+          children:  [
+            const UserAccountsDrawerHeader(
+                accountName: Text("Danilo"),
+                accountEmail: Text("DaniloSacranento@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  child: Text("D"),
+                  backgroundColor: Colors.greenAccent,
+                )
+             ),
+             ListTile(
+              title: Text("Page 1"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                _pageController.jumpToPage(0);
+                Navigator.pop(context);
+                 setState(() {
+              indexBottom = 0;
+            });
+              } ,
+              
+             ),
+              ListTile(
+              title: Text("Page 2"),
+              trailing: Icon(Icons.arrow_forward),
+               onTap: () {
+                _pageController.jumpToPage(1);
+                Navigator.pop(context);
+                 setState(() {
+              indexBottom = 1;
+            });
+                
+               }
+             ),
+              ListTile(
+              title: Text("Page 3"),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                _pageController.jumpToPage(2);
+                Navigator.pop(context);
+                 setState(() {
+              indexBottom = 2;
+            });
+               }
+             )
+          ],
+        ),
+      ),
       body: PageView(
         controller: _pageController,
         children: [
@@ -38,7 +87,8 @@ class _HomePageState extends State<HomePage> {
               indexBottom = page;
             });
             _pageController.animateToPage(page,
-                duration: const Duration(milliseconds: 300), curve: Curves.ease);
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.ease);
           },
           items: const [
             BottomNavigationBarItem(icon: Icon(Icons.add), label: "Intem 1"),
